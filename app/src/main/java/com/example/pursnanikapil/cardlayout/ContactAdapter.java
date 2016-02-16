4424ccbf6ee75e6a044c5e649ea25e6f2bf6fb5a
Kunal Pursnani <Kunal Pursnani>
2016-02-17 01:08:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pursnanikapil/cardlayout/ContactAdapter.java
@@ -0,0 +1,55 @@
+package com.example.pursnanikapil.cardlayout;
+
+/**
+ * Created by Pursnani Kapil on 14-Feb-16.
+ */
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
+
+    private List<ContactInfo> contactList;
+
+    public ContactAdapter(List<ContactInfo> contactList) {
+        this.contactList = contactList;
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(ContactViewHolder contactViewHolder, int i) {
+        ContactInfo ci = contactList.get(i);
+        contactViewHolder.title.setText(ci.name);
+
+    }
+
+    @Override
+    public ContactViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.card_layout, viewGroup, false);
+
+        return new ContactViewHolder(itemView);
+    }
+
+    public static class ContactViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView title;
+
+
+        public ContactViewHolder(View v) {
+            super(v);
+            title =  (TextView) v.findViewById(R.id.title);
+
+        }
+    }
+}
